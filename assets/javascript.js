736523b2a9de3c5c47f72f1f6e028e4cbeea98fb
Mike Fallesen <mfallesen2010@gmail.com>
2020-09-13 22:44:05 -0700

    finish markup for timer, begin working on timer initialization

---
+++ assets/javascript.js
@@ -0,0 +1,20 @@
+// global variables declared here
+
+
+// Question and Answer objects got here
+
+
+// Game initialization script including timer
+let timer = document.querySelector("#timer");
+console.log(timer.innerHTML);
+
+// Print questions and answers to screen
+
+
+// Event listeners for clicks
+
+
+// Validate user input and print to screen
+
+
+// Clear Screen and print new question.
---
+++ assets/questions.js
@@ -0,0 +1,9 @@
+const questions = [];
+
+const question1 = {
+    question: "The external JavaScript file must contain /<script> tag. True or False?",
+    answerOpt1: "True",
+    answerOpt2: "False",
+    correctAns: "False",
+
+};
