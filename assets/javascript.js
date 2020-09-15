1322f2bd115272a646828a36c5ec51165523fc37
Mike Fallesen <mfallesen2010@gmail.com>
2020-09-15 09:46:10 -0700

    updates to all files

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,25 +1,29 @@
 // global variables declared here
+let timer = document.querySelector("#timer");
+let quizBox = document.querySelector("#question-container");
 
 
 // Question and Answer objects got here
-
+const questionMatrix = questions;
 
 // Game initialization script including timer
-let timer = document.querySelector("#timer");
+
 console.log(timer.innerHTML);
 
-let counter = 10;
+let counter = 3; // return to 100 and delete
 function quizCountDown() {
     document.querySelector("header").style.visibility = "visible";
-let timerCountDown = setInterval(function() {
-    console.log(counter);
-    timer.innerHTML = counter
-    counter--;
-    if (counter === -1) {
-        clearInterval(timerCountDown);
-        counter = 10;
-    };
-}, 1000);
+    questionPrinter();
+    let timerCountDown = setInterval(function () {
+        console.log(counter);
+        timer.innerHTML = counter
+        counter--;
+        //clears the timer so it doesn't run past 0
+        if (counter === -1) {
+            clearInterval(timerCountDown);
+            counter = 3; //return to 100 and delete
+        };
+    }, 1000);
 };
 
 // window.addEventListener("click", )
@@ -28,6 +32,21 @@ document.querySelector("#quizStart").addEventListener("click", quizCountDown)
 
 // Print questions and answers to screen
 
+
+
+console.log(questionMatrix[0].question);
+function questionPrinter() {
+    console.log("hi there");
+    quizBox.removeChild(quizBox.firstElementChild);
+    quizBox.firstElementChild.innerHTML = questionMatrix[0].question;
+    for (var i = 0; i < questionMatrix[0].answerOps.length; i++) {
+        let ansBtn = document.createElement("button");
+        let ans = questionMatrix[0].answerOps[i];
+        quizBox.appendChild(ansBtn);
+        ansBtn.innerHTML = ans;
+        
+    }
+};
 
 // Event listeners for clicks
 
--- assets/questions.js
+++ assets/questions.js
@@ -1,7 +1,17 @@
-const questions = [q1, ];
+
+
+
+
 
 const q1 = {
-    question: "The external JavaScript file must contain /<script> tag. True or False?",
+    question: "The external JavaScript file must contain < script > tag. True or False?",
     answerOps: ["True", "False"],
     correctAns: "False",
-};
+}; 
+
+
+
+
+const questions = [q1, ];
+
+// module.exports = {questions: questions};
