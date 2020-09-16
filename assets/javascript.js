f380085c15ffe946e00375f4a820dfb3306ace65
Mike Fallesen <mfallesen2010@gmail.com>
2020-09-15 20:49:58 -0700

    trying to fix button click events

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,23 +1,27 @@
 // global variables declared here
 let timer = document.querySelector("#timer");
 let quizBox = document.querySelector("#question-container");
-
+let score = timer.textContent;
 
 // Question and Answer objects got here
 const questionMatrix = questions;
 
 // Game initialization script including timer
 
-console.log(timer.innerHTML);
+console.log(timer.textContent);
 
 let counter = 3; // return to 100 and delete
 function quizCountDown() {
     document.querySelector("header").style.visibility = "visible";
     document.querySelector("#quizStart").style.visibility = "hidden";
     questionPrinter();
+    counterStart();
+};
+
+function counterStart() {
     let timerCountDown = setInterval(function () {
         console.log(counter);
-        timer.innerHTML = counter
+        timer.textContent = counter
         counter--;
         //clears the timer so it doesn't run past 0
         if (counter === -1) {
@@ -25,7 +29,7 @@ function quizCountDown() {
             counter = 3; //return to 100 and delete
         };
     }, 1000);
-};
+}
 
 
 // Print questions and answers to screen
@@ -33,26 +37,47 @@ function quizCountDown() {
 
 
 console.log(questionMatrix[2].question);
+
+//prints a question and answer buttons to the screen. 
 function questionPrinter() {
-    console.log("hi there");
+    // removes the h1 header
     quizBox.removeChild(quizBox.firstElementChild);
-    quizBox.firstElementChild.innerHTML = questionMatrix[2].question;
+    //populates the question
+    quizBox.firstElementChild.textContent = questionMatrix[2].question;
+    // loops through the question object and creates a button for each answer
     for (var i = 0; i < questionMatrix[2].answerOps.length; i++) {
         let ansBtn = document.createElement("button");
         let ans = questionMatrix[2].answerOps[i];
-        ansBtn.setAttribute("class", "button is-primary");
+        ansBtn.setAttribute("class", "button is-primary answer-button");
         ansBtn.setAttribute("id", i)
         quizBox.appendChild(ansBtn);
-        ansBtn.innerHTML = ans;
+        ansBtn.textContent = ans;
+        
     }
+    
 };
 
 // Event listeners for clicks
 document.querySelector("#quizStart").addEventListener("click", quizCountDown)
 
 // Validate user input and print to screen
+// 
+document.querySelector("#question-container").addEventListener("click", function (event) {
+    console.log("I clicked inside the div");
+    console.log(event);
+    if (event.target.button.matches(".answer-button")) {
+        console.log("this is a button click");
+    }
+})
 
-//if inner.HTML === correctAns
+
+//function ansClick() {
+
+//     document.getElementsByClassName(".answer-button").addEventListener("click", function () {
+//         console.log("clicked");
+//     })
+// };
+
 
 
 // Clear Screen and print new question.
