eef8505e252fa0bd23cb1e094e8d38938fe53f0d
Mike Fallesen <mfallesen2010@gmail.com>
2020-09-16 22:59:01 -0700

    markup and style changes

--- assets/javascript.js
+++ assets/javascript.js
@@ -6,14 +6,16 @@ let qIndex = 0;
 let counter = 30; // return to 100 and delete
 let testActive = false;
 let hsList = window.localStorage;
+let nuDiv = document.createElement("div");
+let nuPar = document.createElement("p");
 // Question and Answer objects got here
 const questionMatrix = questions;
 hsList.setItem("mycat", "Tom");
 console.log(hsList);
+
+
+
 // Game initialization script including timer
-
-
-
 function quizCountDown() {
     //display timer
     document.querySelector("header").style.visibility = "visible";
@@ -25,7 +27,7 @@ function quizCountDown() {
 };
 
 function counterStart() {
-        timerCountDown = setInterval(function () {
+    timerCountDown = setInterval(function () {
         testActive = true;
         console.log(counter);
         timer.textContent = counter
@@ -39,31 +41,19 @@ function counterStart() {
     }, 1000);
 }
 
-// function printScore(){
-
-// }
-
-
-// console.log(questionMatrix[2].question);
-
 //prints a question and answer buttons to the screen. 
 function questionPrinter() {
     //clear HTML from #question-container
     quizBox.firstElementChild.innerHTML = "";
     //create wrapper div
-    let nuDiv = document.createElement("div");
+    nuDiv = document.createElement("div");
     //create empty paragraph
-    let nuPar = document.createElement("p");
+    nuPar = document.createElement("p");
 
 
     if (qIndex > 9) {
         clearInterval(timerCountDown);
-        let score = timer.textContent;
-        let nameInput = document.createElement("input");
-        quizBox.firstElementChild.innerHTML = "";
-        nuPar.textContent = "You got " + score + " points!";
-        nuDiv.appendChild(nuPar);
-        nuDiv.appendChild(nameInput);
+        saveScore();
     } else {
         //set text of new paragraph
         nuPar.textContent = questionMatrix[qIndex].question;
@@ -93,8 +83,7 @@ document.querySelector("#quizStart").addEventListener("click", quizCountDown)
 // Validate user input and print to screen
 // 
 document.querySelector("#question-container").addEventListener("click", function (event) {
-    // console.log("I clicked inside the div");
-    // console.log(event.target.textContent);
+
     if (event.target.textContent === questionMatrix[qIndex].correctAns) {
         console.log("this is a button click");
         qIndex++
@@ -102,28 +91,21 @@ document.querySelector("#question-container").addEventListener("click", function
         //insert next question and update score
     } else {
         console.log("this is the wrong button click")
+        event.target.setAttribute("disabled", true)
         counter -= 10;
     }
 });
 
+function saveScore() {
+    let score = timer.textContent;
+    let nameInput = document.createElement("input");
+    let nameBtn = document.createElement("button")
+    nameBtn.setAttribute("class", "button is-primary")
+    quizBox.firstElementChild.innerHTML = "";
+    nuPar.textContent = "You got " + score + " points!";
+    nuDiv.appendChild(nuPar);
+    nuDiv.appendChild(nameInput);
+    nuDiv.appendChild(nameBtn);
+}
 
 
-
-// loop Through Questions
-
-//  function questionLoop() {
-//     for (var j = 0; j <= questionMatrix.length; j++) 
-
-
-
-
-
-//     // while (testActive === true) {
-//     //     console.log("testActive");
-//     // }
-//  }
-
-// function ()
-
-
-// Keep track of score
